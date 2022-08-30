5939ec721a7c9bbd5256b78e3016be7eedb20cec
Paul McDonald <pmcdonald1012@gmail.com>
2022-08-30 16:54:31 -0400

    fixed cjs error

--- backend/migration.sql
+++ backend/migration.sql
@@ -4,8 +4,9 @@ CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
     username TEXT,
     email TEXT,
-    paswword TEXT,
-    location TEXT,
+    password TEXT,
+    city TEXT,
+    state TEXT,
     picture TEXT
 );
 
@@ -32,3 +33,5 @@ CREATE TABLE posted_items(
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+INSERT INTO users (username, email ,password, city, state, picture) VALUES ('superuser', 'superuser@superuser.com', 'superuser', 'Denver', 'Colorado', 'https://nwsid.net/wp-content/uploads/2015/05/dummy-profile-pic-300x300.png' )
