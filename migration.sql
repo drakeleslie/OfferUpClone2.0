46a68067a1c5049b36efd4179bfdd19ffd6ce07b
Johnwelle11 <johnwelle14@gmail.com>
2022-08-30 18:36:39 -0400

    Worked on getting a header/navigation bar made. I did npm install create-react-app, styled-components, and react-router-dom for trying to get what I was working with to work; shouldn't be a problem though.
    git add .
    git /navigation bar made. I did npm install create-react-app, styled-components, and react-r
    outer-dom for trying to get what I was working with to work; shouldn't be a problem thou
    gh.

---
+++ migration.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS users, saved_items, posted_items;
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    username TEXT,
+    email TEXT,
+    paswword TEXT,
+    location TEXT,
+    picture TEXT
+);
+
+CREATE TABLE saved_items(
+    item_id SERIAL,
+    api_id INTEGER,
+    title TEXT,
+    price TEXT,
+    category TEXT,
+    description TEXT,
+    image TEXT,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE posted_items(
+    posted_item_id SERIAL,
+    api_id INTEGER,
+    title TEXT,
+    price TEXT,
+    category TEXT,
+    description TEXT,
+    image TEXT,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
