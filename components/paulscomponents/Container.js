2bf7883770de10c6dab5db398d2ebfdd23210881
Johnwelle11 <johnwelle14@gmail.com>
2022-08-31 19:10:51 -0400

    some changes

--- components/paulscomponents/Container.js
+++ components/paulscomponents/Container.js
@@ -1,7 +1,12 @@
+import Header from "../johnscomponents/Header";
 const Container = () => {
   return (
-    <div>Container</div>
-  )
-}
-
-export default Container
+    <div className="flex flex-col w-full min-h-screen text-black">
+      <Header />
+      <div className="flex grow flex-col">
+        <main className="w-full max-w-screen-2xl my-0 mr-auto ml-auto"></main>
+      </div>
+    </div>
+  );
+};
+export default Container;
