a8fbd405db7dd527b595cdde8a108355ec4d0582
Paul McDonald <pmcdonald1012@gmail.com>
2022-09-01 15:29:13 -0400

    commit

--- components/paulscomponents/ViewItemContainer.js
+++ components/paulscomponents/ViewItemContainer.js
@@ -1,10 +1,12 @@
 import Header from "../johnscomponents/Header"
 import ViewItem from "./ViewItem"
+import styles from "../../styles/ViewItem.module.css"
 const ViewItemContainer = () => {
   return (
     <div className="flex flex-col w-full min-h-screen text-black">
         <Header /> 
         <div className="flex grow flex-col">
+        <div className={styles.spacer}></div>
             <main className="w-full max-w-screen-2xl my-0 mr-auto ml-auto">
               <ViewItem />
             </main>
