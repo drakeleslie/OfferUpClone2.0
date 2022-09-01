c3d6f2eb2843d2a0648e957c2807e0b9e207756b
Johnwelle11 <johnwelle14@gmail.com>
2022-09-01 18:06:57 -0400

    worked on the search bar min

--- components/johnscomponents/Header.js
+++ components/johnscomponents/Header.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import NavBar from "./NavBar";
 import Head from "next/head";
+import styles from "../../styles/header.module.css";
+
 const Header = () => {
   return (
     <div>
+      <img src="offeruplogo.png" width="75" height="75" />
       <NavBar />
     </div>
   );
 };
 
 export default Header;
+// className={styles.title}
