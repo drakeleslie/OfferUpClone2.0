19404df0fca766f2d378b0c290968ba21b3ea575
Johnwelle11 <johnwelle14@gmail.com>
2022-08-31 22:06:30 -0400

    something

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express, { response } from "express";
 import { pool } from "./database.js";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 
-
-dotenv.config(); 
+dotenv.config();
 
 //config server
 const server = express();
@@ -13,35 +12,36 @@ server.use(express.json());
 server.use(cors());
 //routes
 server
-//get request for checking login credentials
-  .get('/api/authenticate', (req, res) => {
-    pool.query("SELECT * FROM users "), (err, result)=>{
-      res.send(result)
-  }
-})
-
-  .post('/api/authenticate', async (req, res) => {
-    const getUserAndPassword = (
-      await pool.query(`SELECT * FROM users WHERE username = $1 AND password = $2;`, 
-      [req.body.username,req.body.password])); // query db for input username and password
-
-        if(getUserAndPassword.rows.length === 0){ // if rows is empty, username and password combo does not exist
-          res.send(false)
-          console.log("in server -- user is NOT authorized")
-        } else {
-          res.send(true)
-          console.log("in server -- user is authorized")
-        }
-    })
- })
-//to get saved items by user id 
-  .get('/api/saved', async (req, res) => {
-    console.log(`Recieved Saved Request: ${req.body}`)
-    pool.query("SELECT * FROM saved_items").then((data) => {
-      res.send(data.rows)  
-    })
-    
+  //get request for checking login credentials
+  .get("/api/authenticate", (req, res) => {
+    pool.query("SELECT * FROM users "),
+      (err, result) => {
+        res.send(result);
+      };
   })
+
+  .post("/api/authenticate", async (req, res) => {
+    const getUserAndPassword = await pool.query(
+      `SELECT * FROM users WHERE username = $1 AND password = $2;`,
+      [req.body.username, req.body.password]
+    ); // query db for input username and password
+
+    if (getUserAndPassword.rows.length === 0) {
+      // if rows is empty, username and password combo does not exist
+      res.send(false);
+      console.log("in server -- user is NOT authorized");
+    } else {
+      res.send(true);
+      console.log("in server -- user is authorized");
+    }
+  })
+  //to get saved items by user id
+  .get("/api/saved", async (req, res) => {
+    console.log(`Recieved Saved Request: ${req.body}`);
+    pool.query("SELECT * FROM saved_items").then((data) => {
+      res.send(data.rows);
+    });
+  });
 server.listen(port, () => {
-    console.log(`Express server is running on port: ${port}`)
-})
+  console.log(`Express server is running on port: ${port}`);
+});
--- components/johnscomponents/Header.js
+++ components/johnscomponents/Header.js
@@ -2,13 +2,24 @@ import Link from "next/link";
 import NavBar from "./NavBar";
 const Header = () => {
   return (
-    <header
-      className="MuiPaper-root MuiAppBar-root MuiAppBar-positionFixed
-      MuiAppBar-colorPrimary jss26 jss18 mui-fixed MuiPaper-elevation4"
-    >
-      <NavBar />
-    </header>
-  );
+    <div>
+      <Header>
+        <title>This page has a title 🤔</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Header>
+
+      <h1>This page has a title 🤔</h1>
+    </div>
+  )
+  //   <header
+  //     className="MuiPaper-root MuiAppBar-root MuiAppBar-positionFixed
+  //     MuiAppBar-colorPrimary jss26 jss18 mui-fixed MuiPaper-elevation4"
+  //   >
+  //     <NavBar />
+  //   </header>
+  // );
 };
 
 export default Header;
+
