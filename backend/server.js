ad0e72954871ef4ebccf05cbf4fcf8a67670e4c8
Matthew Mickler <matthewmickler@Matthews-MacBook-Pro.local>
2022-09-02 14:42:32 -0700

    nothing

--- backend/server.js
+++ backend/server.js
@@ -46,9 +46,6 @@ server.post('/api/newUser', async (req, res) => {
 
 
 });
-
-
-
 //to get saved items by user id
 server.get("/api/saved/:user_id", async (req, res) => {
   const userId = req.params.user_id;
