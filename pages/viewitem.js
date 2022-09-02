bc341920ae902574996add8ffa0d6e902f808af0
Paul McDonald <pmcdonald1012@gmail.com>
2022-09-02 16:10:03 -0400

    added features to viewitem page

--- pages/viewitem.js
+++ pages/viewitem.js
@@ -1,8 +1,14 @@
+import Head from "next/head"
 import  ViewItemContainer  from "../components/paulscomponents/ViewItemContainer"
 
 const viewitem = () => {
   return (
     <div>
+      <Head>
+        <title>View Item</title>
+        <script src="https://kit.fontawesome.com/da6e8f7c59.js" crossorigin="anonymous"></script>
+     </Head> 
+
       <ViewItemContainer />
     </div>
   )
