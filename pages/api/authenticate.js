1682594a1e49e76294156baec92e1f931bbbacb3
Paul McDonald <pmcdonald1012@gmail.com>
2022-09-06 14:53:43 -0400

    a

---
+++ pages/api/authenticate.js
@@ -0,0 +1,16 @@
+import pool from "../../backend/database"
+export default function authenticate(req, res) {
+    console.log(req.params);
+    const username = req.params.username;
+  
+    const password = req.params.password 
+    const getUserNameAndPassword = ( await pool.query("SELECT * FROM users WHERE (username = $1 AND password = $2);", [username, password]))
+    let checkForUserID = getUserNameAndPassword.rows[0].user_id
+    if(!checkForUserID){
+      res.send(false)
+    } else{
+      res.send({bool: true, data: getUserNameAndPassword.rows})
+  
+    }
+  
+}
