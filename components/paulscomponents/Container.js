4ecc62418f90b6e5cb7605912b7e72a0dabf171e
Johnwelle11 <johnwelle14@gmail.com>
2022-09-07 16:02:51 -0400

    tweaked the header

--- components/paulscomponents/Container.js
+++ components/paulscomponents/Container.js
@@ -1,22 +1,21 @@
+import Header from "../johnscomponents/Header";
+import HomeComp from "./HomeComp";
+import styles from "../../styles/Home.module.css";
+import Footer from "../mattscomponents/Footer";
 
-import Header from "../johnscomponents/Header"
-import HomeComp from "./HomeComp"
-import styles from "../../styles/Home.module.css"
-import Footer from "../mattscomponents/Footer"
-
-const Container = ({currentUser}) => {
+const Container = ({ currentUser }) => {
   return (
-    <div className="flex flex-col w-full min-h-screen text-black">
-        <Header currentUser={currentUser}/> 
-        <div className="flex grow flex-col">
-          <div className={styles.spacer}></div>
-            <main className="w-full max-w-screen-2xl my-0 mr-auto ml-auto">
-              <HomeComp />
-            </main>
-        </div>
-        <Footer/>
+    <div className="flex flex-col min-h-screen text-black w-full w-screen">
+      <Header currentUser={currentUser} />
+      <div className="flex grow flex-col">
+        <div className={styles.spacer}></div>
+        <main className="w-full max-w-screen-2xl my-0 mr-auto ml-auto">
+          <HomeComp />
+        </main>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Container
+export default Container;
