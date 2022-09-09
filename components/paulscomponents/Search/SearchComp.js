4fd668561170ae3ddb9d6f82cab41c59da795283
Paul McDonald <pmcdonald1012@gmail.com>
2022-09-09 17:27:26 -0400

    More deployment issues

--- components/paulscomponents/Search/SearchComp.js
+++ components/paulscomponents/Search/SearchComp.js
@@ -1,4 +1,4 @@
-import styles from "../../styles/Home.module.css"
+import styles from "../../../styles/Home.module.css"
 import SearchProduct from "./SearchProduct"
 const SearchComp = ({data}) => {
 
