57423743ca8ea1daa4844dd8270f010f84ab029e
drakeleslie <drake.j.leslie@gmail.com>
2022-09-13 11:05:30 -0500

    small tweak

--- pages/sellerprofiles.js
+++ pages/sellerprofiles.js
@@ -1,12 +1,12 @@
-import SellerProfile from "../components/drakescomponents/SellerProfile";
-import React, { useEffect, useState } from "react";
+// import SellerProfile from "../components/drakescomponents/SellerProfile";
+// import React, { useEffect, useState } from "react";
 
-const sellerProfiles = () => {
-  return (
-    <>
-      <SellerProfile />
-    </>
-  );
-};
+// const sellerProfiles = () => {
+//   return (
+//     <>
+//       <SellerProfile />
+//     </>
+//   );
+// };
 
-export default sellerProfiles;
+// export default sellerProfiles;
